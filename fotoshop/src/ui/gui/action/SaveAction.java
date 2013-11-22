002901b3b67e476e63f7824a2ac269c30aa1ddcc
ledenis <ledenis.git@gmail.com>
2013-11-22 21:16:17 +0000

    Add SaveAction

---
+++ fotoshop/src/ui/gui/action/SaveAction.java
@@ -0,0 +1,30 @@
+package ui.gui.action;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
+import fotoshop.Editor;
+
+public class SaveAction extends GUIAction {
+	private static final long serialVersionUID = 1L;
+	
+	public SaveAction(Component parent, Editor editor) {
+		super(parent, editor);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ev) {
+		JFileChooser fileChooser = new JFileChooser();
+
+		int result = fileChooser.showSaveDialog(parent);
+		if (result == JFileChooser.APPROVE_OPTION) {
+			File file = fileChooser.getSelectedFile();
+
+			editor.saveImage(file);
+		}
+	}
+
+}
