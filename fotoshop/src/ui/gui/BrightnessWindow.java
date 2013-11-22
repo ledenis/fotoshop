afd1cc88db462a63cfd422a20354d094e860d2d3
ledenis <ledenis.git@gmail.com>
2013-11-22 21:00:32 +0000

    Add few components to BrightnessWindow

--- fotoshop/src/ui/gui/BrightnessWindow.java
+++ fotoshop/src/ui/gui/BrightnessWindow.java
@@ -1,6 +1,10 @@
 package ui.gui;
 
+import java.awt.Container;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JSlider;
 
 /** This window should appear when the user wants to set a brightness filter.
  * It shows a slider that controls the brightness level of the filter.
@@ -9,6 +13,12 @@ public class BrightnessWindow extends JDialog {
 	private static final long serialVersionUID = 1L;
 
 	public BrightnessWindow() {
+		Container contentPane = getContentPane();
 		
+		JSlider slider = new JSlider();
+		JButton okButton = new JButton("Ok");
+		
+		contentPane.add(slider);
+		contentPane.add(okButton);
 	}
 }
