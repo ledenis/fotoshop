98757e3825adca0e1b0d93b46f2827a73aca92b2
ledenis <ledenis.git@gmail.com>
2013-11-23 17:01:35 +0000

    Rework the BrightnessWindow layout

--- fotoshop/src/ui/gui/BrightnessWindow.java
+++ fotoshop/src/ui/gui/BrightnessWindow.java
@@ -2,23 +2,55 @@ package ui.gui;
 
 import java.awt.Container;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-/** This window should appear when the user wants to set a brightness filter.
- * It shows a slider that controls the brightness level of the filter.
+/**
+ * This window should appear when the user wants to set a brightness filter. It
+ * shows a slider that controls the brightness level of the filter.
  */
 public class BrightnessWindow extends JDialog {
 	private static final long serialVersionUID = 1L;
 
+	private static final String TEXT = "Adjust the brightness: ";
+
+	private JLabel label;
+	private JSlider slider;
+
 	public BrightnessWindow() {
 		Container contentPane = getContentPane();
-		
-		JSlider slider = new JSlider();
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+
+		label = new JLabel();
+		label.setAlignmentX(CENTER_ALIGNMENT);
+		slider = new JSlider();
+		slider.setMajorTickSpacing(10);
+		slider.setPaintTicks(true);
+		updateText();
 		JButton okButton = new JButton("Ok");
-		
+		okButton.setAlignmentX(CENTER_ALIGNMENT);
+
+		contentPane.add(label);
 		contentPane.add(slider);
 		contentPane.add(okButton);
+		
+		// action listener
+		slider.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				updateText();
+			}
+		});
+
+		pack();
+	}
+	
+	private void updateText() {
+		label.setText(TEXT + slider.getValue() + "%");
 	}
 }
